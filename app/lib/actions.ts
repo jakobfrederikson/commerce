7397ed32d32d6a034fc09d5c7c1270a39444ce4d
Jakob <jakobfred1999@gmail.com>
2023-12-04 15:05:47 +1300

    fix: get products based on category or search term

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 import * as TypeDefs from '@/app/lib/definitions';
 import { unstable_noStore as noStore } from 'next/cache';
 
-export async function GetAllProducts() {
+async function GetAllProducts() {
     noStore();
     try {
         const response = await fetch('https://fakestoreapi.com/products');
@@ -10,5 +10,42 @@ export async function GetAllProducts() {
     } catch ( error ) {
         console.error("HTTP Error:", error);
         throw new Error("Failed to retrieve all products.");
+    }    
+};
+
+export async function getProducts({
+    category,
+    searchTerm
+}: {
+    category?: string;
+    searchTerm?: string;
+}): Promise<TypeDefs.Product[]> {
+    console.log("category: ", category);
+    console.log("searchTerm: ", searchTerm);
+    try {
+        const allProducts = await GetAllProducts();
+
+        if (!category && !searchTerm) {
+            return allProducts;
+        }
+
+        let filteredProducts = allProducts;
+
+        if (category) {
+            filteredProducts = filteredProducts.filter(product => product.category.toLowerCase() === category.toLowerCase());
+        }
+
+        if (searchTerm) {
+            const lowercasedSearchTerm = searchTerm.toLowerCase();
+            filteredProducts = filteredProducts.filter(product => 
+                product.title.toLowerCase().includes(lowercasedSearchTerm) ||
+                product.description.toLowerCase().includes(lowercasedSearchTerm)
+            );
+        }
+
+        return filteredProducts;
+    } catch (error) {
+        console.error("Error: ", error);
+        throw new Error("Failed to retrieve products.");
     }    
 };
